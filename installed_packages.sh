d4346aacf746b92d117921c718e2458218f93154
Lars Liedtke <lars@familie-liedtke.net>
2018-02-01 12:52:42 +0100

    added script for querying packages installed after base installation

---
+++ installed_packages.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+comm -13 \
+  <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort) \
+  <(comm -23 \
+    <(dpkg-query -W -f='${Package}\n' | sed 1d | sort) \
+    <(apt-mark showauto | sort) \
+  )
